4ec60c5f9615d4647500a856345582e857fdc0e2
Lucas Outor Barros <lucasoutor@gmail.com>
2020-11-01 18:02:06 -0300

    criando serializers e dando update em models

--- Marvelapi/Marvel/models.py
+++ Marvelapi/Marvel/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Character(models.Model):
-    name  = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     comics = models.ForeignKey('Comics', on_delete=models.CASCADE)
     events = models.ForeignKey('Event', on_delete=models.CASCADE)
     series = models.ForeignKey('Series', on_delete=models.CASCADE)
