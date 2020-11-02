bd5db17314d2293336df9551785c4bc5211d2ec2
Lucas Outor Barros <lucasoutor@gmail.com>
2020-11-02 12:53:32 -0300

    Alteração

---
+++ MARVELDEXTRA/CharactherMarvel/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CharacthermarvelConfig(AppConfig):
+    name = 'CharactherMarvel'
--- MARVELDEXTRA/CharactherMarvel/models.py
+++ MARVELDEXTRA/CharactherMarvel/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Character(models.Model):
+class Characther(models.Model):
     name = models.CharField(max_length=100)
     comics = models.ForeignKey('Comics', on_delete=models.CASCADE)
     events = models.ForeignKey('Event', on_delete=models.CASCADE)
